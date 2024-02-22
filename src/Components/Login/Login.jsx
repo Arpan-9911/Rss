869a47ef63f2f87aad10e5326f54a5c68719a456
Arpan-9911 <arpanks1263@gmail.com>
2024-02-22 16:51:30 +0530

    Login Page Margins

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -3,9 +3,9 @@ import profile from '../../assets/userProfile.png'
 
 const Login = () => {
   return (
-    <div className="w-screen h-fit flex items-center justify-center py-32">
+    <div className="w-screen h-screen flex items-center justify-center">
       <div>
-        <form className="flex bg-gray-400 shadow-xl shadow-gray-600 p-10 flex-col items-center w-fit justify-center rounded-2xl" onLoad={"reset"}>
+        <form className="flex bg-gray-400 shadow-xl shadow-gray-600 -mt-20 p-10 flex-col items-center w-fit justify-center rounded-2xl" onLoad={"reset"}>
           <img src={profile} alt="Login" className="w-16 h-16 mb-4 text-white" />
           <input type="text" name="username" id="username" placeholder="Username" className="w-52 text-black outline-none rounded-lg px-2 py-1 mb-2" />
           <input type="password" name="password" id="password" placeholder="Password" className="w-52 text-black outline-none h-8 rounded-lg px-2 py-1 mb-5" />
