8e106c0d1b6a3a0c261a3808696bdfd99c5e9e0c
Arpan-9911 <arpanks1263@gmail.com>
2024-02-22 21:28:16 +0530

    Mail Page Aligned

--- src/Components/Contact/Mail.jsx
+++ src/Components/Contact/Mail.jsx
@@ -38,8 +38,8 @@ const Mail = () => {
   }
 
   return (
-    <div className="w-screen h-screen flex items-center justify-center -mt-20">
-      <form onSubmit={handleSubmit} className="flex flex-col rounded-2xl p-10 bg-gray-400 shadow-xl shadow-gray-700 mdmax:p-5">
+    <div className="w-screen h-screen flex items-center justify-center">
+      <form onSubmit={handleSubmit} className="flex -mt-20 flex-col rounded-2xl p-10 bg-gray-400 shadow-xl shadow-gray-700 mdmax:p-5">
         <h1 className="m-auto text-xl text-red-600 font-bold">Mail Us</h1>
         <input type="text" name="user_name" placeholder="Name" autoComplete='off' className="my-2 w-80 mdmax:w-48 h-10 rounded-lg p-2 placeholder:text-gray-600" value={name} onChange={(e) => setName(e.target.value)} />
         <input type="email" name="user_email" placeholder='Email' autoComplete='off' className="my-2 w-80 mdmax:w-48 h-10 rounded-lg p-2 placeholder:text-gray-600" value={email} onChange={(e) => setEmail(e.target.value)} />
