1cbf88b1ddbbbf8b726750cbbc5639300e1b33a9
Arpan-9911 <arpanks1263@gmail.com>
2024-02-26 22:33:09 +0530

    App Layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Outlet } from 'react-router-dom'
 import Navbar from './Pages/Navbar'
 import Home from './Pages/Home'
 import Footer from './Pages/Footer'
@@ -9,18 +9,31 @@ import Mail from './Components/Contact/Mail'
 import About from './Pages/About'
 import QrPage from './Pages/QrPage'
 
+const AppLayout = () => (
+  <div>
+    <Navbar />
+    <div className='min-h-screen'>
+      <Outlet />
+    </div>
+    <Footer />
+  </div>
+)
+
 function App() {
 
   return (
-    <Router>
+    <Router basename='/rssassociates'>
       <Navbar/>
       <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/login' element={<LoginPage/>} />
-        <Route path='/contact' element={<ContactPage/>} />
-        <Route path='/mail' element={<Mail/>} />
-        <Route path='/about' element={<About/>} />
-        <Route path='/consultationfee' element={<QrPage/>} />
+        <Route path='/' element={<AppLayout/>}>
+          <Route path='/' element={<Home/>} />
+          <Route path='/login' element={<LoginPage/>} />
+          <Route path='/contact' element={<ContactPage/>} />
+          <Route path='/mail' element={<Mail/>} />
+          <Route path='/about' element={<About/>} />
+          <Route path='/consultationfee' element={<QrPage/>} />
+          <Route path='*' element={<Home/>} />
+        </Route>
       </Routes>
       <Footer/>
     </Router>
