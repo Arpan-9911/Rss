cbebe81a3ea5f2d3f8ccb17dc8c356257175017c
Arpan-9911 <arpanks1263@gmail.com>
2024-02-26 23:25:33 +0530

    Outlet Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Outlet } from 'react-router-dom'
 import Navbar from './Pages/Navbar'
 import Home from './Pages/Home'
 import Footer from './Pages/Footer'
